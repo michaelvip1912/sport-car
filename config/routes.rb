0bbfa1f442681624344965c6a57d5deda62a8c22
yangw <michaelvip1991@gmail.com>
2015-02-27 17:58:21 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
 
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
 end
