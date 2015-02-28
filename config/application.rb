270bcd74ce3500f979cbe4bfff78edbda4689f76
yangw <michaelvip1991@gmail.com>
2015-02-28 18:06:15 +0800

    change nav

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module SportCar
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    I18n.enforce_available_locales = true
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
